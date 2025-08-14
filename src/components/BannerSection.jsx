cff5a4b9a1a1f7ab86547c35bb4d86f766a1d50a
Seemab Ahmed <seemab40615@gmail.com>
2025-08-15 01:00:25 +0500

    fix

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -39,7 +39,7 @@ export const HeroBannerSection =({
             )}
 
             {/* Title */}
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-extrabold text-white mb-6 leading-tight">
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold text-white mb-6 leading-tight">
               {title}
             </h1>
 
