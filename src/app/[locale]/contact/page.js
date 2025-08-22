1bd80739e75e31b92489f352001b2645b1ca4be9
seemab-ahmed <seemab40615@gmail.com>
2025-08-22 18:00:44 +0500

    NewsPage

--- src/app/[locale]/contact/page.js
+++ src/app/[locale]/contact/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const page = () => {
+export default function page (){
   return (
     <div>Contact page</div>
   )
--- src/app/[locale]/news/page.js
+++ src/app/[locale]/news/page.js
@@ -1,7 +1,23 @@
-import React from 'react'
 
-export const page = () => {
+"use client"
+import BlogList from '@/components/Bloglist';
+import BlogSearch from '@/components/BlogSearch';
+import { useState } from 'react';
+
+export default function page (){
+ const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    console.log(term);
+    setSearchTerm(term);
+  };
+
   return (
-    <div>News Page</div>
-  )
+    <>
+    <BlogSearch
+     description="Are you interested in the latest developments at the RODOPI Group? This page provides access to our newest press releases and company news. Stay up to date on major announcements, corporate events, and our group’s latest achievements."
+     onSearch={handleSearch} />
+    <BlogList searchTerm={searchTerm} />
+    </>
+  );
 }
